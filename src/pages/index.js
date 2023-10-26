599ad5953c1a19ab903984b27b32e60ff5144e25
Jack Bush <bush.jack@gmail.com>
2023-10-26 18:45:50 +0100

    url params working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,42 @@
-import React, {useState, useEffect} from "react"
-import Layout from '../components/layout'
-import {scenarios} from '../content'
-
+import React, { useState, useEffect } from "react";
+import Layout from "../components/layout";
+import { scenarios } from "../content";
+import { useQueryParamString } from "react-use-query-param-string";
 
 export default function HomePage() {
-  const [scenario, setScenario] = useState(false);
-  
-  const setRandomScenario = () => {
-    setScenario(scenarios[Math.floor(Math.random() * scenarios.length)])
+  const getRandomScenarioIdx = () => {
+    return Math.floor(Math.random() * scenarios.length)
   }
 
-  // grab a random one on load
+  const [scenario, setScenario] = useState(false);
+  const [scenarioIdxParam, setScenarioIdxParam, initialized] =
+    useQueryParamString("scenario", getRandomScenarioIdx(), false);
+
+  // SHONK this shouldn't be needed
+  let i = scenarioIdxParam;
+
+  const loadScenario = (ev) => {
+    if (ev) {
+      // lazy check if it's been triggered by button
+      i = getRandomScenarioIdx()
+    } else {
+      // has been triggered by useEffect
+      i = scenarioIdxParam;
+    }
+    setScenarioIdxParam(i);
+    setScenario(scenarios[i]);
+  };
+
+  // load a scenario
   useEffect(() => {
-    setRandomScenario();
-  }, []);
+    if(initialized) loadScenario();
+  }, [initialized]);
 
   return (
     <Layout>
       <h1>Inclusive design scenarios</h1>
       <p>{scenario}</p>
-      <button onClick={setRandomScenario}>I need more</button>
+      <button onClick={loadScenario}>I need more</button>
     </Layout>
-  )
+  );
 }

4ddeb2d6c2d12e578d3590a7baeb34a9857fdf1a
Jack Bush <bush.jack@gmail.com>
2023-10-26 18:45:37 +0100

    prettier

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,34 +2,34 @@ import * as React from "react";
 import { Link } from "gatsby";
 
 const fontsWrapper = {
-  fontFamily: "Inter"
-}
+  fontFamily: "Inter",
+};
 
 const navStyles = {
-  position: 'fixed',
+  position: "fixed",
   top: 0,
   left: 0,
-  width: '100vw'
-}
+  width: "100vw",
+};
 
 const navItemsStyles = {
-  listStyle: 'none',
+  listStyle: "none",
   margin: 0,
   padding: 0,
-  textAlign: 'right'
-}
+  textAlign: "right",
+};
 
 const navItemStyles = {
-  display: 'inline-block',
-  padding: 12
-}
+  display: "inline-block",
+  padding: 12,
+};
 
 const containerStyles = {
-  width: '90vw',
+  width: "90vw",
   maxWidth: 720,
   margin: "auto",
   marginTop: 100,
-}
+};
 
 export default function Layout({ children }) {
   return (
@@ -37,15 +37,14 @@ export default function Layout({ children }) {
       <nav style={navStyles}>
         <ul style={navItemsStyles}>
           <li style={navItemStyles}>
-            <Link
-              to="/"
-            >Home</Link>
+            <Link to="/">Home</Link>
           </li>
-          <li style={navItemStyles}><Link to="/about">About</Link></li>
+          <li style={navItemStyles}>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </nav>
       <main style={containerStyles}>{children}</main>
     </div>
   );
 }
-
